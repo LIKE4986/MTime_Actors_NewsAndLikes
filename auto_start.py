44985acdbdc9912bb33a8d9493c6811546d68e01
LIKE4986 <87894986@qq.com>
2018-07-19 17:38:04 +0800

    start

---
+++ auto_start.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+import os
+
+error_flags = False
+
+if __name__ == '__main__':
+    trytime = 0
+    while(error_flags == False):
+        trytime = trytime+1
+        print("第",str(trytime),"次尝试")
+        os.system('python3 ./LikesAndNewsCollection.py')
+    print("第", str(trytime), "次尝试", "任务结束")
